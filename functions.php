6372e528bec657e8aae648a16a65d0aad2096092
Frank Barrera <frankb2320@gmail.com>
2016-01-20 11:38:26 -0430

    test de version 2

--- functions.php
+++ functions.php
@@ -442,6 +442,8 @@ add_filter( 'xmlrpc_methods', function( $methods ) {
 } );*/
 
 
+
+
 // set_site_transient('update_themes', null);
 function geko_check_update( $transient ) {
 $usergithub=farkbarn;
@@ -452,21 +454,22 @@ $ramastable=master;
     $theme_data = wp_get_theme(wp_get_theme()->template);
     $theme_slug = $theme_data->get_template();
     //Delete '-master' from the end of slug
-    $theme_uri_slug = preg_replace('/-".$ramastable."$/', '', $theme_slug);
-    $remote_version = '0.0.0';
-    $style_css = wp_remote_get("https://raw.githubusercontent.com/".$usergithub."/".$theme_uri_slug."/".$ramastable."/style.css")['body'];
-    if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
-        $remote_version = _cleanup_header_comment( $match[1] );
-    if (version_compare($theme_data->version, $remote_version, '<')) {
-        $transient->response[$theme_slug] = array(
-            'theme'       => $theme_slug,
-            'new_version' => $remote_version,
-            'url'         => 'https://github.com/".$usergithub."/'.$theme_uri_slug,
-            'package'     => 'https://github.com/".$usergithub."/'.$theme_uri_slug.'/archive/".$ramastable.".zip',
-        );
-    }
-    return $transient;
+    $theme_uri_slug = preg_replace('/-master$/', '', $theme_slug);
+   $remote_version = '0.0.0';
+   $style_css = wp_remote_get("https://raw.githubusercontent.com/".$usergithub."/".$theme_uri_slug."/master/style.css")['body'];
+   if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
+       $remote_version = _cleanup_header_comment( $match[1] );
+   if (version_compare($theme_data->version, $remote_version, '<')) {
+       $transient->response[$theme_slug] = array(
+           'theme'       => $theme_slug,
+           'new_version' => $remote_version,
+           'url'         => 'https://github.com/'.$usergithub.'/'.$theme_uri_slug,
+           'package'     => 'https://github.com/'.$usergithub.'/'.$theme_uri_slug.'/archive/master.zip',
+       );
+   }
+   return $transient;
 }
 add_filter( 'pre_set_site_transient_update_themes', 'geko_check_update' );
+
 
 ?>
