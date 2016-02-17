2cad1e207db9be1c63a2552b4a6ed3236e3d9273
Frank Barrera <frankb2320@gmail.com>
2016-02-17 09:10:19 -0430

    cambio de ads videoslide

--- footer.php
+++ footer.php
@@ -10,14 +10,5 @@
 					</footer>
 			<div class="espacio10"></div>
 <?php wp_footer(); ?>
-
-<?php /*VIDEO SLIDER*/ ?>
-<script type="text/javascript">
-var ut_params = ut_params || [];ut_params.push("UT-170718233");//version:0.4
-(function() {var ut = document.createElement('script'); ut.type = 'text/javascript'; ut.async = true;
-ut.src = (("https:" == document.location.protocol) ? "https://" : "http://") + 'app.ubertags.com/javascripts/ubertags.js';
-var script = document.getElementsByTagName('script')[0]; script.parentNode.insertBefore(ut, script);})();
-</script>
-
 	</body>
 </html>
--- index.php
+++ index.php
@@ -315,4 +315,14 @@ $val=rand(1, 10);
 					</div>
 					<div class="limpiar"></div>
 					<div class="espacio05"></div>
+
+<?php /*VIDEO SLIDER*/ ?>
+<script type="text/javascript">
+var ut_params = ut_params || [];ut_params.push("UT-170718233");//version:0.4
+(function() {var ut = document.createElement('script'); ut.type = 'text/javascript'; ut.async = true;
+ut.src = (("https:" == document.location.protocol) ? "https://" : "http://") + 'app.ubertags.com/javascripts/ubertags.js';
+var script = document.getElementsByTagName('script')[0]; script.parentNode.insertBefore(ut, script);})();
+</script>
+
 <?php get_footer(); ?>
+
