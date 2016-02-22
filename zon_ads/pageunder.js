3cc65782d5d11daad07f698caaa8d77cc68cb007
Frank Barrera <frankb2320@gmail.com>
2016-02-22 08:16:09 -0430

    pageunder y vSlider

--- zon_ads/pageunder.js
+++ zon_ads/pageunder.js
@@ -1,6 +1,7 @@
-<script type="text/javascript">
-var ut_params = ut_params || [];ut_params.push("UT-170718233");//version:0.4
-(function() {var ut = document.createElement('script'); ut.type = 'text/javascript'; ut.async = true;
-ut.src = (("https:" == document.location.protocol) ? "https://" : "http://") + 'app.ubertags.com/javascripts/ubertags.js';
-var script = document.getElementsByTagName('script')[0]; script.parentNode.insertBefore(ut, script);})();
-</script>
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WMV2TC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMV2TC');</script>
