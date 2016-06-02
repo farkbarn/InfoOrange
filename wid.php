c65157bad22d7cd770e3ef9c25ed454a80b0c84f
farkbarn <frankbarrerag@gmail.com>
2016-06-01 21:43:10 -0400

    agregando captura de ancho

---
+++ wid.php
@@ -0,0 +1,26 @@
+<?php
+if(session_status() == PHP_SESSION_NONE){session_start();}
+if(isset($_POST['width']) && isset($_POST['height'])) {
+    $_SESSION['wid']=$_POST['width'];
+    $_SESSION['hei']=$_POST['height'];
+    $_SESSION['attwid']=true;
+
+	if(isset($_COOKIE['wscr']))
+	{
+		if (($_SESSION['attwid']) && ($_SESSION['wid']<>$_COOKIE['wscr']))
+		{
+			$_SESSION['attwid']=false;
+			unset($_COOKIE['wscr']);
+			setcookie('wscr',$_SESSION['wid'],time()+365*24*60*60,'/','*');
+		}
+	}
+	else
+	{
+		setcookie('wscr',$_SESSION['wid'],time()+365*24*60*60,'/','*');
+	}
+
+    echo json_encode(array('val'=>true,'wid'=>$_SESSION['wid'],'hei'=>$_SESSION['hei'],'wscr'=>$_COOKIE['wscr']));
+} else {
+    echo json_encode(array('val'=>false));
+}
+?>
